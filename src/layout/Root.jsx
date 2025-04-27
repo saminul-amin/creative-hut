49ff933a9aaa5f3738b26511254044e8a2d84fbd
Md. Saminul Amin <saminul.amin@gmail.com>
2025-04-28 01:57:47 +0600

    first commit

---
+++ src/layout/Root.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/NavBar";
+import Home from "../pages/Home";
+
+export default function Root() {
+    return <div>
+        <Navbar />
+        <Home />
+        <Footer />
+    </div>
+}
